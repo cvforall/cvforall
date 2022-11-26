d75a3ece08c620a7dd06c96b952b24b35ef4bcc3
xFaZe341 <86230524+xFaZe341@users.noreply.github.com>
2022-11-26 20:18:46 +0400

    add empty test_function in cvforall.py

--- cvforall/cvforall.py
+++ cvforall/cvforall.py
@@ -1,5 +1,5 @@
 def convert(my_name):
-  """
+    """
   Print a line about converting a notebook.
   Args:
       my_name (str): person's name
@@ -7,4 +7,8 @@ def convert(my_name):
       None
   """
 
-  print(f"I'll convert a notebook for you some day, {my_name}.")
+    print(f"I'll convert a notebook for you some day, {my_name}.")
+
+
+def test_function():
+    pass
